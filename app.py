66b9e557482059b650fbbfa7a515dab52cf51c48
HarryG <harryguajan1@gmail.com>
2025-07-19 20:38:39 -0500

    Si algo deja de funcionar volver a este commit :c

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 from algorithms.analyzer import CyberbullyingAnalyzer
 from algorithms.selector import AlgorithmSelector
+##Comentario Harry
 
 app = Flask(__name__)
 CORS(app)  # Permitir solicitudes desde el frontend
